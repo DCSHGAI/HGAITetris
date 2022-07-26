4643d9c2810284c7545ab40b8a5f0ca832a76910
DCSHGAI <104516996+DCSHGAI@users.noreply.github.com>
2022-07-26 13:38:59 -0600

    Temporarily disabled Tamer

--- Tetris/StateEvaluation.py
+++ Tetris/StateEvaluation.py
@@ -13,7 +13,6 @@ class Tamer2:
         self.compiled  = False
         self.model     = None
         self.runRandom = False
-
         self.compileModel()
         self.record       = []
         self.arecord      = []
@@ -170,16 +169,16 @@ def GameStateEvaluation(game,events):
     global tamer
     global gameSym
     # GameState gives you access to the entire Tetris class and includes the field and all controls
-    if tamer==None:
-        tamer   = Tamer2(game.width)
-        if game.width == 10:
-            tamer.load_weights(checkPointPath)
-        gameSym = ts.TetrisSym(game.height,game.width)
+    #if tamer==None:
+    #    tamer   = Tamer2(game.width)
+    #    if game.width == 10:
+    #        tamer.load_weights(checkPointPath)
+    #    gameSym = ts.TetrisSym(game.height,game.width)
     
-    if gameSym.width != game.width:
-        tamer   = Tamer2(game.width)
-        gameSym = ts.TetrisSym(game.height,game.width)
-        
+    #if gameSym.width != game.width:
+    #    tamer   = Tamer2(game.width)
+    #    gameSym = ts.TetrisSym(game.height,game.width)
+    #    
     #LOOP OVER INPUT EVENTS - INSERT YOUR OWN CONTROLS HERE
     for event in events:
         if event.type == pygame.KEYDOWN:
