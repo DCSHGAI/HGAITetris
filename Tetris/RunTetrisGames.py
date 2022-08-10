3e63daf6f21c2b9d3e609429cdb470402839ed16
Bryce Bartlett <brycebartlett001@gmail.com>
2022-08-10 00:24:09 -0400

    Compiled

--- Tetris/RunTetrisGames.py
+++ Tetris/RunTetrisGames.py
@@ -17,10 +17,10 @@ def StartGames():
         if  platform.system() == "Windows":
             print("The program has detected that you are running Windows and will run the appropriate command to spool up Tetris games.")
             time.sleep(1)
-            subprocess.Popen(".\Tetris.py " + str(Count) , shell=True)
+            subprocess.Popen(".\Tetris.pyc " + str(Count) , shell=True)
         else:
             print("The program has detected that you are running Linux or Mac and will run the appropriate command to spool up Tetris games.")
             time.sleep(1)
-            subprocess.Popen("#!/usr/bin/env python Tetris.cpython-39.pyc " + str(Count) , shell=True)
+            subprocess.Popen("#!/usr/bin/env python Tetris.pyc " + str(Count) , shell=True)
 
 #StartGames()
